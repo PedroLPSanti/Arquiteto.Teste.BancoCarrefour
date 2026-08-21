c3529f433b2a08d6d9b981a708516d56228c8faa
Pedro Santi <pedro.santi@tolife.com.br>
2026-08-21 17:45:50 -0300

    corrigindo a versao do service subiu com o cache

--- Carrefour.API.BusinessIntelligence/Services/DailyConsolidatedService.cs
+++ Carrefour.API.BusinessIntelligence/Services/DailyConsolidatedService.cs
@@ -1,22 +1,13 @@
 ﻿using Carrefour.API.BusinessIntelligence.DTOs;
-using Carrefour.API.BusinessIntelligence.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Distributed;
-using System.Linq;
-using System.Text.Json;
 
 namespace Carrefour.API.BusinessIntelligence.Repositories
 {
     public class DailyConsolidatedService : IDailyConsolidatedService
     {
         private readonly IDailyConsolidatedRepository _repository;
-        private readonly IDistributedCache _cache;
 
-        private const string CacheKey = "daily_consolidated_ledger";
-
-        public DailyConsolidatedService(IDailyConsolidatedRepository repository, IDistributedCache cache)
+        public DailyConsolidatedService(IDailyConsolidatedRepository repository)
         {
-            _cache = cache;
             _repository = repository;
         }
 
@@ -24,31 +15,8 @@ namespace Carrefour.API.BusinessIntelligence.Repositories
             CancellationToken ct = default
         )
         {
-            string? cachedData = await _cache.GetStringAsync(CacheKey, ct);
-
-            if (!string.IsNullOrEmpty(cachedData))
-            {
-                var cachedDailyConsolidatedDTO = JsonSerializer.Deserialize<IEnumerable<DailyConsolidatedDTO>>(cachedData);
-                if (cachedDailyConsolidatedDTO != null)
-                {
-                    return cachedDailyConsolidatedDTO;
-                }
-            }
-
             var dailyConsolidatedList = await _repository.ReadAllAsync(ct);
             var dailyConsolidatedDTO = dailyConsolidatedList.Select(p => new DailyConsolidatedDTO(p));
-
-            if (dailyConsolidatedDTO.Any())
-            {
-                string serializedData = JsonSerializer.Serialize(dailyConsolidatedDTO);
-
-                var cacheOptions = new DistributedCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1)
-                };
-
-                await _cache.SetStringAsync(CacheKey, serializedData, cacheOptions, ct);
-            }
 
             return dailyConsolidatedDTO;
         }
